1ce4ee05d51d6b715293765ad6cf84923d3344b2
JoowonPark0910 <dlqfursla@gmail.com>
2026-04-28 15:13:17 +0900

    feat: add place autocomplete to my tickets form

--- frontend/app/[locale]/(main)/my-tickets/my-tickets.types.ts
+++ frontend/app/[locale]/(main)/my-tickets/my-tickets.types.ts
@@ -19,6 +19,7 @@ export interface IssuedTicket {
 export interface TicketForm {
     name: string;
     venue: string;
+    googlePlaceId: string;
     validDate: string;
     totalCount: string;
     allowDuplicate: boolean;
